dfda10ff3b7aada0385b6cb7fec07da91ec81567
Christopher Kenny <ctk44@cornell.edu>
2021-10-25 00:58:56 -0400

    svc

--- R/svc.R
+++ R/svc.R
@@ -1,7 +1,35 @@
-svc <- function(plans, dvs) {
+#' Make a Seats-Votes Curve
+#'
+#' @param plans redist_plans object
+#' @param dvs
+#' @param swing
+#'
+#' @return ggplot2 object
+#' @export
+#'
+#' @examples
+#' library(redist)
+#' data(iowa)
+#' ia <- redist_map(iowa, existing_plan = cd_2010, pop_tol = 0.01)
+#' plans <- redist_smc(ia, 100, silent = TRUE)
+#' plans <- plans %>% mutate(dvs = group_frac(ia, dem_08, dem_08 + rep_08))
+#' svc(plans, dvs)
+#'
+svc <- function(plans, dvs, swing = svc_unif) {
+  dvs <- rlang::enquo(dvs)
+
+  sv <- swing(plans, !!dvs)
+
+  ref <- plans %>%
+    redist::subset_ref() %>%
+    pull(draw) %>%
+    unique()
+
+  sv %>%
+    filter(!draw %in% ref) %>%
+    ggplot(aes(x = votes, y = seats, group = draw)) +
+    geom_line(alpha = 0.05) +
+    theme_bw() +
+    geom_line(data = sv %>% filter(draw %in% ref), aes(color = draw))
 
 }
-
-# plans_svc <- function(plans, dvs) {
-#
-# }
---
+++ R/swings.R
@@ -0,0 +1,24 @@
+svc_unif <- function(plans, dvs) {
+  dvs <- rlang::enquo(dvs)
+  #dvs <- if (rlang::is_quosure(dvs)) dvs else rlang::enquo(dvs)
+  plans %>%
+    as_tibble() %>%
+    group_by(draw) %>%
+    group_modify(~.x %>% pull(!!dvs) %>% swing_unif())
+}
+
+
+# swing helpers ----
+
+swing_unif <- function(dvs) {
+  swings <- seq(-1, 1, by = 0.0025)
+  votes <- vapply(swings, function(x){
+    mean(dvs - x)
+  }, 0)
+  seats <- vapply(swings, function(x){
+    mean(dvs - x > 0.5)
+  }, 0)
+  seats <- seats[votes >=0 & votes <= 1]
+  votes <- votes[votes >=0 & votes <= 1]
+  tibble::tibble(seats = seats, votes = votes)
+}
